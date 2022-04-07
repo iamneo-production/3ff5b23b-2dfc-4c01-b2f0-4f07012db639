3c876e9588ace29633500801fc536c3e07ff577b
Boopathiraj B-19cs024 <87652313+Boopathi05@users.noreply.github.com>
2022-04-07 19:19:37 +0530

    Update App.test.js

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -1,8 +1,42 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cart from './Cart';
+import Login from './Login';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path='/login'>
+            <Login/>
+
+          </Route>
+          <Route path='/signup'>
+            <h1>Hi</h1>
+          </Route>
+          <Route path='/cart'>
+            <Cart />
+            
+
+          </Route>
+
+
+          <Route path='/'>
+            <Home />
+
+          </Route>
+
+        </Switch>
+
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
